098d44eed0781511555fe8bf4ae859298f148b09
TheSoundOfWA <121793003+TheSoundOfWA@users.noreply.github.com>
2024-02-09 09:54:22 +0800

    Update ybt1272-code.cpp

--- ybt1272-code.cpp
+++ ybt1272-code.cpp
@@ -8,20 +8,20 @@ int dp[N];
 vector<int> v[N];
 
 int main() {
-    int v, n, t;
-	scanf("%d%d%d", &v, &n, &t);
+	int V, n, t;
+	scanf("%d%d%d", &V, &n, &t);
 	for (int i = 1; i <= n; i++) {
-        int p;
+		int p;
 		scanf("%d%d%d", &w[i], &c[i], &p);
 		v[p].push_back(i);
 	}
 
 	for (int i = 1; i <= t; i++)
-		for (int j = v; j >= 0; j--)
+		for (int j = V; j >= 0; j--)
 			for (int k = 0; k < v[i].size(); k++)
 				if (w[v[i][k]] <= j)
 					dp[j] = max(dp[j], dp[j - w[v[i][k]]] + c[v[i][k]]);
-	printf("%d", dp[v]);
+	printf("%d", dp[V]);
 
 	return 0;
 }
